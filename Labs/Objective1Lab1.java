88f339eb27ad323bb9ff975da652cfebd6870f02
Zachary Dechant <zdechant0621@gmail.com>
2023-09-15 10:43:48 -0600

    Objective 1

--- Labs/Objective1Lab1.java
+++ Labs/Objective1Lab1.java
@@ -1,7 +1,5 @@
 public class Objective1Lab1 {
   public static void main(String[] args) {
-    /*
-      "Hello, Skill Distillery;"
-    */
+     System.out.println("Hello, Skill Distillery");
   }
 }
