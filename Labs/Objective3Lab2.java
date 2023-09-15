9dbbc84eab90f3b6fe507bb7aa6cbc16dca70097
Zachary Dechant <zdechant0621@gmail.com>
2023-09-15 15:49:18 -0600

    Hypotenuse

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,11 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+      double side1 = 10;
+      double side2 = 8;
+      double result = (side1 * side1) + (side2 * side2);
+      double hypotenuse = Math.sqrt(result);
+      System.out.println(hypotenuse);
+  
+
+  }
+}
