6daa986f221c8810e1cffc3f359511a11fe5dc70
Zachary Dechant <zdechant0621@gmail.com>
2023-09-15 18:32:53 -0600

    Year, Month, Weekend?

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,11 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int year = 1990; 
+		String month = "January";
+		Boolean Weekend = false;
+
+		System.out.println(year);
+		System.out.println(month);
+		System.out.println(Weekend);
+	}
+}
