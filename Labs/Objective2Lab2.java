b145690b0d73372e605ac7769a46a3afa22c25fd
Zachary Dechant <zdechant0621@gmail.com>
2023-09-15 18:08:46 -0600

    Name, Age, State

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Zachary Dechant";
+		int age = 33;
+		String state = "Colorado";
+
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(state);
+	}
+}
