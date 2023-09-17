2e3629b2c1c8b199bd821c3865b5551336067b00
Zachary Dechant <zdechant0621@gmail.com>
2023-09-17 10:07:00 -0600

    Random Number

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,12 @@
+public class Objective3Lab3 {
+	public static void main (String[] args) {
+		int first, second, third, randomnumber;
+
+		first = 1 + (int)(Math.random() * 6);
+		second = 1 + (int)(Math.random() * 6);
+		third = 1 + (int)(Math.random() * 6);
+
+		randomnumber = (first + second + third);
+		System.out.println(randomnumber);
+	}
+}
