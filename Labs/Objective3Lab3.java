698d26d1a03783d60067b556af9171c95d467edd
Zachary Dechant <zdechant0621@gmail.com>
2023-09-17 11:03:17 -0600

    Correction to Code

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -2,11 +2,11 @@ public class Objective3Lab3 {
 	public static void main (String[] args) {
 		int first, second, third, randomnumber;
 
-		first = 1 + (int)(Math.random() * 6);
+	    first = 1 + (int)(Math.random() * 6);
 		second = 1 + (int)(Math.random() * 6);
-		third = 1 + (int)(Math.random() * 6);
+        third = 1 + (int)(Math.random() * 6);
 
 		randomnumber = (first + second + third);
-		System.out.println(randomnumber);
+		System.out.println(first + " + " + second + " + " + third + " = " + randomnumber);
 	}
 }
