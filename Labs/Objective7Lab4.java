a31a5c11cb6aa9eee894d4dd99880464532e8b1b
Zachary Dechant <zdechant0621@gmail.com>
2023-09-19 15:41:39 -0600

    Sum Numbers

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+    public static void main(String[] args){
+    int total = 0, counter = 0;
+
+        while (counter <=20) {
+            total = total + counter;
+            ++counter;
+        }
+         System.out.println(total);
+    }
+
+}
