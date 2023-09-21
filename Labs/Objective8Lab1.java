5abd17eb41dfb9e853293ddc84af81e12390b01e
Zachary Dechant <zdechant0621@gmail.com>
2023-09-21 09:56:43 -0600

    Printing Numbers

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int i=1; i<=10; i+=1){
+      System.out.println(i);
+    }
+
+    for (int j=10; j>=1; j-=1){
+      System.out.println(j);
+    }
+
+  }
+}
