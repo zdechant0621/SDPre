33d03ced1663078f90a028b448d1f59c86be5071
Zachary Dechant <zdechant0621@gmail.com>
2023-09-21 11:08:39 -0600

    Print Message

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,13 @@
+public class Objective9Lab2 {
+  public static void main(String[] args) {
+    System.out.println("In main");
+    printMessage();
+    System.out.println("In main");
+  }
+
+  public static void printMessage() {
+    System.out.print("In Method");
+
+
+  }
+}
