811822c5372f4dfbc75dd3912542fc8b9116fe9e
Zachary Dechant <zdechant0621@gmail.com>
2023-09-21 11:28:37 -0600

    Correction to Code

--- Labs/Objective9Lab3.java
+++ Labs/Objective9Lab3.java
@@ -13,22 +13,14 @@ public class Objective9Lab3 {
 
       if (selection == 1)
         System.out.println("Hello World");
-        System.out.println("_____Menu_____");
-        System.out.println("1: Say Hello");
-        System.out.println("2: List My favorite foods");
-        System.out.println("3: Exit");
-        System.out.println();
       
+      printMenu();
       selection = scanner.nextInt();
 
       if (selection == 2)
         System.out.println("Apple, Banana, Coconut");
-        System.out.println("_____Menu_____");
-        System.out.println("1: Say Hello");
-        System.out.println("2: List My favorite foods");
-        System.out.println("3: Exit");
-        System.out.println();
       
+      printMenu();
       selection = scanner.nextInt();
 
       if (selection == 3)
